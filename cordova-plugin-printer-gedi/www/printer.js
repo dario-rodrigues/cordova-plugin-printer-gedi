2e957cf21ec64ef990e6a580a16bbc63478c002e
dario-rodrigues <58831936+dario-rodrigues@users.noreply.github.com>
2019-12-12 22:17:01 -0300

    Delete printer.js

--- cordova-plugin-printer-gedi/www/printer.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var GEDI = function () {};
-
-GEDI.print = function (content) {
-    exec(null, null, 'Printer', 'print', [content]);
-};
-
-module.exports = GEDI;
