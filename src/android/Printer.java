5f3aacac35d0a184fb83ba683409eb6847f679f3
dario-rodrigues <58831936+dario-rodrigues@users.noreply.github.com>
2019-12-13 22:18:17 -0300

    Add files via upload

--- src/android/Printer.java
+++ src/android/Printer.java
@@ -77,24 +77,16 @@ public class Printer extends CordovaPlugin {
          @Override
          public void run() {
             try {
-			   iGedi = GEDI.getInstance(getApplicationContext());
+			   iGedi = GEDI.getInstance(Printer.this);
 			   
                IPRNTR iPrntr = iGedi.getPRNTR();
 
-               tPRNTR.DrawPicture(getApplicationContext(), iPrntr,"CENTER",0,50,160,
-                       "logo");
-
-               tPRNTR.DrawBlankLine(20, iPrntr);
-
-               tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
+               tPRNTR.DrawString(this, iPrntr, "CENTER", 0, 0, "NORMAL",
                        false, false, false, 17, viaCliente);
-
-               tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
-                       true, false, false, 17, "______________________________________");
 
                tPRNTR.DrawBlankLine(10, iPrntr);
 
-               tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
+               /*tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
                        true, false, false, 17, pedidoComprovante);
 
                tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
@@ -108,7 +100,7 @@ public class Printer extends CordovaPlugin {
                tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
                        true, false, false, 20, "SENHA: " + Integer.toString(senha) );
 
-               tPRNTR.DrawBlankLine(140, iPrntr);
+               tPRNTR.DrawBlankLine(140, iPrntr);*/
             }catch (Exception e){
             }
          }
