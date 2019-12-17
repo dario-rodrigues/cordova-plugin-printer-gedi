ffe59dea3abc429bebe0e39e220dc239056f81f3
dario-rodrigues <58831936+dario-rodrigues@users.noreply.github.com>
2019-12-17 11:52:51 -0300

    Add files via upload

--- src/android/gedi.java
+++ src/android/gedi.java
@@ -23,7 +23,7 @@ import br.com.gertec.gedi.structs.GEDI_PRNTR_st_StringConfig;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class gedi extends Activity 
+public class gedi extends CordovaPlugin 
 {
 	private IGEDI iGedi = null;
 
@@ -33,63 +33,66 @@ public class gedi extends Activity
         if ( action.equals( "print" ) ) 
 		{
             String texto = args.getString( 0 );
-            this.print( texto, callbackContext );
+            this.printer.print( texto, callbackContext );
             return true;
         }
 		
         return false;
     }
-
-    private void print( String texto, CallbackContext callbackContext ) 
-	{
-        if ( texto != null && texto.length( ) > 0 ) 
+	
+	public class printer extends Activity 
+    {
+		private void print( String texto, CallbackContext callbackContext ) 
 		{
-			Thread t = new Thread( ) 
+			if ( texto != null && texto.length( ) > 0 ) 
 			{
-				@Override
-				public void run( ) 
+				Thread t = new Thread( ) 
 				{
-					try 
+					@Override
+					public void run( ) 
 					{
-					   iGedi = GEDI.getInstance( getApplicationContext( ) );
-					   
-					   IPRNTR iPrntr = iGedi.getPRNTR( );
+						try 
+						{
+						   iGedi = GEDI.getInstance( getApplicationContext( ) );
+						   
+						   IPRNTR iPrntr = iGedi.getPRNTR( );
 
-					   tPRNTR.DrawString( getApplicationContext( ), iPrntr, "CENTER", 0, 0, "NORMAL", false, false, false, 17, texto );
+						   tPRNTR.DrawString( getApplicationContext( ), iPrntr, "CENTER", 0, 0, "NORMAL", false, false, false, 17, texto );
 
-					   /*tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
-							   true, false, false, 17, "______________________________________");
+						   /*tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
+								   true, false, false, 17, "______________________________________");
 
-					   tPRNTR.DrawBlankLine(10, iPrntr);
+						   tPRNTR.DrawBlankLine(10, iPrntr);
 
-					   tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
-							   true, false, false, 17, pedidoComprovante);
+						   tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
+								   true, false, false, 17, pedidoComprovante);
 
-					   tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
-							   true, false, false, 17, getResources().getString(R.string.nota));
+						   tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
+								   true, false, false, 17, getResources().getString(R.string.nota));
 
-					   tPRNTR.DrawBarCode( iPrntr, GEDI_PRNTR_e_BarCodeType.QR_CODE, 300, 300, "www.ger7.com.br" );
+						   tPRNTR.DrawBarCode( iPrntr, GEDI_PRNTR_e_BarCodeType.QR_CODE, 300, 300, "www.ger7.com.br" );
 
-					   Random rn = new Random();
-					   int senha = rn.nextInt(1000) + 1;
+						   Random rn = new Random();
+						   int senha = rn.nextInt(1000) + 1;
 
-					   tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
-							   true, false, false, 20, "SENHA: " + Integer.toString(senha) );
+						   tPRNTR.DrawString(getApplicationContext(), iPrntr, "CENTER", 0, 0, "NORMAL",
+								   true, false, false, 20, "SENHA: " + Integer.toString(senha) );
 
-					   tPRNTR.DrawBlankLine(140, iPrntr);*/
-					   callbackContext.success( texto );
-					} catch ( Exception ex )
-					{
-						ex.printStackTrace( );
-						callback.error( ex.getMessage( ) );
+						   tPRNTR.DrawBlankLine(140, iPrntr);*/
+						   callbackContext.success( texto );
+						} catch ( Exception ex )
+						{
+							ex.printStackTrace( );
+							callback.error( ex.getMessage( ) );
+						}
 					}
-				}
-			};
-			
-			t.start( );            
-        } else 
-		{
-            callbackContext.error( "Texto invalido para impressao." );
-        }
-    }
+				};
+				
+				t.start( );            
+			} else 
+			{
+				callbackContext.error( "Texto invalido para impressao." );
+			}
+		}
+	}
 }
