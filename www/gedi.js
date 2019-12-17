df2fc62fdc7a235485e4413613061a37b63bddf2
dario-rodrigues <58831936+dario-rodrigues@users.noreply.github.com>
2019-12-17 11:11:23 -0300

    Add files via upload

--- www/gedi.js
+++ www/gedi.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'gedi', 'coolMethod', [arg0]);
+exports.print = function (arg0, success, error) {
+    exec(success, error, 'gedi', 'print', [arg0]);
 };
