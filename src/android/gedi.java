527a64ff57143c66b4831b3b7010e9e4af356eb0
dario-rodrigues <58831936+dario-rodrigues@users.noreply.github.com>
2019-12-17 11:11:50 -0300

    Add files via upload

--- src/android/gedi.java
+++ src/android/gedi.java
@@ -7,6 +7,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Activity;
+
+import br.com.gertec.gedi.GEDI;
+import br.com.gertec.gedi.enums.GEDI_PRNTR_e_Alignment;
+import br.com.gertec.gedi.enums.GEDI_PRNTR_e_BarCodeType;
+import br.com.gertec.gedi.enums.GEDI_PRNTR_e_Status;
+import br.com.gertec.gedi.exceptions.GediException;
+import br.com.gertec.gedi.interfaces.IGEDI;
+import br.com.gertec.gedi.interfaces.IPRNTR;
+import br.com.gertec.gedi.structs.GEDI_PRNTR_st_BarCodeConfig;
+import br.com.gertec.gedi.structs.GEDI_PRNTR_st_PictureConfig;
+import br.com.gertec.gedi.structs.GEDI_PRNTR_st_StringConfig;
+
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -14,19 +27,19 @@ public class gedi extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if (action.equals("print")) {
+            String texto = args.getString(0);
+            this.print(texto, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
+    private void print(String texto, CallbackContext callbackContext) {
+        if (texto != null && texto.length() > 0) {
+            callbackContext.success(texto);
         } else {
-            callbackContext.error("Expected one non-empty string argument.");
+            callbackContext.error( "Texto invalido para impressao." );
         }
     }
 }
